39382d2a4114db3c6dbec0de575108262b890b01
Ubuntu <ubuntu@ip-172-31-86-31.ec2.internal>
2024-06-24 15:08:37 +0000

    new commit

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, this is a simple web application'
+    return 'Hello, this is a simple web application for ci-cd pipeline'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
