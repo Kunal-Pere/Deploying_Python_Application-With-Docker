3645ea503f586f2a1ba871fade874f220c19e6ae
Ubuntu <ubuntu@ip-172-31-95-172.ec2.internal>
2024-06-24 08:42:05 +0000

    new change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, this is a simple web application from kpz software pvt. ltd.'
+    return 'Hello, this is a simple web application from Ajay Dabe (Sr. DevOps engineers)'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
